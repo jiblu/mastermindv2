3ba0362842f620f368d3c6b94f27153aa43bbe05
ji <jkim0107@gmail.com>
2020-01-30 18:17:45 -0800

    added remote to local

--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { Colors, Metrics } from '../Themes/index'
@@ -16,44 +16,52 @@ const MidContainer = styled.div`
   flex-grow: 6
 `
 
-const Dashboard = props => {
-  return (
-    <Stack justify='space-between'>
-      <SideContainer>
-        <Text
-          size='medium'
-          color='secondary'
-          align='center'
-        >
-          Game Stats
-        </Text>
-        <TextDisplay text='Username' value={props.username} />
-        <TextDisplay text='Level' value={props.level} />
-        <TextDisplay text='Score' value={props.score} />
-        <TextDisplay text='Guesses Left' value={props.guessesLeft} />
-        <TextDisplay text='Secret Code' value={props.secretCode} />
-      </SideContainer>
-      <MidContainer>
-        <Input
-          text='Current Guess'
-          id='currentGuess'
-          placeholder='please enter four digit number'
-          // onChangeHandler={this.onChangeHandler.bind(this)}
-          // value={this.state.currentGuess}
-        />
-      </MidContainer>
-      <SideContainer>
-        <Text
-          size='medium'
-          color='secondary'
-          align='center'
-        >
-          Your Guesses
-        </Text>
-        some right content
-      </SideContainer>
-    </Stack>
-  )
+class Dashboard extends Component {
+
+  handleChange (id, e) {
+    console.log(id)
+  }
+
+  render () {
+    return (
+      <Stack justify='space-between'>
+        <SideContainer>
+          <Text
+            size='medium'
+            color='secondary'
+            align='center'
+          >
+            Game Stats
+          </Text>
+          <TextDisplay text='Username' value={this.props.username} />
+          <TextDisplay text='Level' value={this.props.level} />
+          <TextDisplay text='Score' value={this.props.score} />
+          <TextDisplay text='Guesses Left' value={this.props.guessesLeft} />
+          <TextDisplay text='Secret Code' value={this.props.secretCode} />
+        </SideContainer>
+        <MidContainer>
+          <Input
+            text='Current Guess'
+            id='currentGuess'
+            placeholder='please enter four digit number'
+            onChange={this.handleChange.bind(this)}
+            // onChangeHandler={this.onChangeHandler.bind(this)}
+            // value={this.state.currentGuess}
+          />
+        </MidContainer>
+        <SideContainer>
+          <Text
+            size='medium'
+            color='secondary'
+            align='center'
+          >
+            Your Guesses
+          </Text>
+          some right content
+        </SideContainer>
+      </Stack>
+    )
+  }
 }
 
 const mapStateToProps = state => {
