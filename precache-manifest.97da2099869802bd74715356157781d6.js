f2ae03d6dd7034337fdf778acb6778f3911b5da2
ji <jkim0107@gmail.com>
2020-01-31 17:23:49 -0800

    Updates

---
+++ precache-manifest.97da2099869802bd74715356157781d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b675ce66639d7825d581e4b77a20c124",
+    "url": "/mastermindv2/index.html"
+  },
+  {
+    "revision": "3d0e83ad2f10040d67f5",
+    "url": "/mastermindv2/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "e778b1b866a77bfef2e1",
+    "url": "/mastermindv2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d0e83ad2f10040d67f5",
+    "url": "/mastermindv2/static/js/2.8088500d.chunk.js"
+  },
+  {
+    "revision": "8bd4535e836bdcaa11a978803b8e9724",
+    "url": "/mastermindv2/static/js/2.8088500d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e778b1b866a77bfef2e1",
+    "url": "/mastermindv2/static/js/main.3fcfc99e.chunk.js"
+  },
+  {
+    "revision": "d6cba1f90e4c7b5b2854",
+    "url": "/mastermindv2/static/js/runtime-main.ffcaf099.js"
+  }
+]);
