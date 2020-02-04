b46bfb453a730cb00a2bb089ad8b939436128820
ji <jkim0107@gmail.com>
2020-02-03 20:06:12 -0800

    added middleware to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const PORT = 3000
 
-app.use(express.static(__dirname + '/../client/dist'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../public'))
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
